cf1bedd4a5102c7634c4c98a6c6f29d540d607d1
Arunabha-NOTE <arunabham2017@gmail.com>
2025-07-01 13:21:56 +0530

    update to application properties

---
+++ src/main/java/model/Organization.java
@@ -0,0 +1,23 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "organizations")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Organization {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,  unique = true)
+    private String name;
+}
